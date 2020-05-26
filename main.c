48b625481b567a7894d4a95b841ee620b7226b67
HeeyNick <nikitos15032002@gmail.com>
2020-05-26 22:54:28 +0700

    Some change got merge

--- main.c
+++ main.c
@@ -18,11 +18,11 @@ int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
 
-    const int min = min_element(arr, 10);
+    const int min = min_element(arr, N_ELEMENTS(arr));
 
     printf("Array: ");
     int i;
-    for (i = 0; i < 10; ++i) {
+    for (i = 0; i < N_ELEMENTS; ++i) {
         printf("%d ", arr[i]);
     }
     printf("\n");
